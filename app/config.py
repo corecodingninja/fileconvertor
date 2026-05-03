3f2294170bc3d473075cca44ed2f07bb58f8ff98
corecodingninja <corecodingninja@gmail.com>
2026-05-04 03:26:47 +0500

    initial commit

--- app/config.py
+++ app/config.py
@@ -26,7 +26,7 @@ class BaseConfig:
     SITE_NAME = 'ConverterHub'
     SUPPORT_EMAIL = os.getenv('SUPPORT_EMAIL', 'corecodingninja@gmail.com')
     GOOGLE_ANALYTICS_ID = os.getenv('GOOGLE_ANALYTICS_ID', 'G-6NNFF8M7FY')
-    ADSENSE_CLIENT = os.getenv('ADSENSE_CLIENT', 'ca-pub-XXXXXXXXXXXXXXXX') # Placeholder
+    ADSENSE_CLIENT = os.getenv('ADSENSE_CLIENT', 'ca-pub-5635730488302512')
 
 
 class DevelopmentConfig(BaseConfig):
